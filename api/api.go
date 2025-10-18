2bd3d94ae29b51c6060c1fd6bb1cb51e6d46ef7a
andrew <andrew@yasinsky.com>
2025-10-18 13:30:24 -0700

    PROXY PLUGIN LISTINGS

--- api/api.go
+++ api/api.go
@@ -126,8 +126,8 @@ func (api *API) NewRouter() *web.Service {
 	service.Delete("/api/v1/users/{userId}", api.DeleteUser())
 	service.Post("/api/v1/users/{userId}/toggle-active", api.ToggleUserActive())
 
-	// Plugin schema endpoints (v2)
-	service.Get("/api/v2/plugins", api.GetPluginSchemas())
+	// Plugin schema endpoints
+	service.Get("/api/v1/plugins", api.GetPluginSchemas())
 
 	// API documentation
 	service.Docs("/v1/docs", swgui.New)
